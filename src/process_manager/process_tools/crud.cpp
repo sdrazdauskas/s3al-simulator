179fac36767f8b33404cd51afa9dd69a0157636b
Augustas Turka <augustasturka@gmail.com>
2025-10-12 16:14:45 +0300

    add initial tool stubs, basic crud for now

---
+++ src/process_manager/process_tools/crud.cpp
@@ -0,0 +1,21 @@
+#include "crud.h"
+
+void createProcess(Process& p) {
+    (void)p; //w
+    // TODO: implement feature
+}
+
+void readProcess(const Process& p) {
+    (void)p;
+    // TODO: implement feature
+}
+
+void updateProcess(Process& p) {
+    (void)p;
+    // TODO: implement feature
+}
+
+void deleteProcess(Process& p) {
+    (void)p;
+    // TODO: implement feature
+}
---
+++ src/process_manager/process_tools/crud.h
@@ -0,0 +1,9 @@
+struct Process;
+
+void createProcess(Process& p);
+
+void readProcess(Process& p);
+
+void updateProcess(Process& p);
+
+void deleteProcess(Process& p);
