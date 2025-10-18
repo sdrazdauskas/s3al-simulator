bc63844689b004ee86a4d23a2f3a5f2bbc7f4cbb
sdrazdauskas <180286095+sdrazdauskas@users.noreply.github.com>
2025-10-18 22:19:09 +0300

    remove testing classes

--- src/ProcessManager/OutputTest/CPUScheduler.cpp
+++
@@ -1,8 +0,0 @@
-#include "CPUScheduler.h"
-#include <iostream>
-
-void CPUScheduler::execute_process(int pid, int cpu_time_needed) {
-    std::cout << "[CPUScheduler] Command to execute process "
-              << pid << " for " << cpu_time_needed
-              << " received.\n";
-}
