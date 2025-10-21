b412703687f1abdbf3d15f198d893da8a81b5098
Šerelis Lukas <lukas.serelis@mif.stud.vu.lt>
2025-10-21 16:44:04 +0300

    refactor the storage namespace into a class

--- src/storage/Storage.h
+++ src/storage/Storage.h
@@ -2,38 +2,43 @@
 
 #include <iostream>
 #include <memory>
-#include <string>
 #include <sstream>
+#include <string>
 #include <vector>
 
-namespace storage {
-struct File {
-    std::string name;
-    std::string content;
+class StorageManager {
+public:
+    StorageManager();
+
+    void createFile(const std::string& name);
+    void deleteFile(const std::string& name);
+    void writeFile(const std::string& name);
+    void readFile(const std::string& name);
+    void editFile(const std::string& name);
+    void makeDir(const std::string& name);
+    void removeDir(const std::string& name);
+    void changeDir(const std::string& name);
+    void listDir() const;
+    void printWorkingDir() const;
+
+private:
+    struct File {
+        std::string name;
+        std::string content;
+    };
+
+    struct Folder {
+        std::string name;
+        std::vector<std::unique_ptr<File>> files;
+        std::vector<std::unique_ptr<Folder>> subfolders;
+        Folder* parent = nullptr;
+    };
+
+    std::unique_ptr<Folder> root;
+    Folder* currentFolder;
+
+    int findFileIndex(const std::string& name) const;
+    int findFolderIndex(const std::string& name) const;
+    void recursiveDelete(Folder& folder);
+    void sendToKernel(const std::string& message) const;
 };
-
-struct Folder {
-    std::string name;
-    std::vector<std::unique_ptr<File>> files;
-    std::vector<std::unique_ptr<Folder>> subfolders;
-    Folder* parent = nullptr;
-};
-
-extern Folder root;
-extern Folder* currentFolder;
-
-void sendToKernel(const std::string& message);
-
-void createFile(const std::string& name);
-void deleteFile(const std::string& name);
-void writeFile(const std::string& name);
-void readFile(const std::string& name);
-void editFile(const std::string& name);
-void makeDir(const std::string& name);
-void removeDir(const std::string& name);
-void changeDir(const std::string& name);
-void listDir();
-void printWorkingDir();
-void handleCommand(const std::string& command);
-
-}  // namespace storage
