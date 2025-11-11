7b2f55313fbb86cf1140fc8b7af422a505a1b122
sdrazdauskas <180286095+sdrazdauskas@users.noreply.github.com>
2025-11-11 19:47:19 +0200

    add support kb, gb, mb for --memory

--- src/config/Config.cpp
+++ src/config/Config.cpp
@@ -12,28 +12,37 @@ bool Config::parseArgs(int argc, char* argv[], Config& config) {
             config.verbose = true;
         } 
         else if ((arg == "--memory" || arg == "-m") && i + 1 < argc) {
-            // Parse memory size (supports KB, MB, GB suffixes)
+            // Parse memory size (supports K/KB, M/MB, G/GB suffixes)
             std::string mem_str = argv[++i];
             try {
                 size_t multiplier = 1;
-                char suffix = std::toupper(mem_str.back());
                 
-                switch (suffix) {
-                    case 'K':
-                        multiplier = 1024;
-                        mem_str.pop_back();
-                        break;
-                    case 'M':
-                        multiplier = 1024 * 1024;
-                        mem_str.pop_back();
-                        break;
-                    case 'G':
-                        multiplier = 1024 * 1024 * 1024;
-                        mem_str.pop_back();
-                        break;
-                    default:
-                        // No suffix, treat as bytes
-                        break;
+                // Remove trailing 'B' if present (KB->K, MB->M, GB->G)
+                if (!mem_str.empty() && std::toupper(mem_str.back()) == 'B') {
+                    mem_str.pop_back();
+                }
+                
+                // Check for single letter suffix (K, M, G)
+                if (!mem_str.empty()) {
+                    char suffix = std::toupper(mem_str.back());
+                    
+                    switch (suffix) {
+                        case 'K':
+                            multiplier = 1024;
+                            mem_str.pop_back();
+                            break;
+                        case 'M':
+                            multiplier = 1024 * 1024;
+                            mem_str.pop_back();
+                            break;
+                        case 'G':
+                            multiplier = 1024 * 1024 * 1024;
+                            mem_str.pop_back();
+                            break;
+                        default:
+                            // No suffix, treat as bytes
+                            break;
+                    }
                 }
                 
                 config.memory_size = std::stoull(mem_str) * multiplier;
@@ -63,14 +72,14 @@ void Config::showHelp(const char* program_name) {
     std::cout << "\n";
     std::cout << "Options:\n";
     std::cout << "  -v, --verbose          Enable verbose logging to console\n";
-    std::cout << "  -m, --memory SIZE      Set memory size (e.g., 512K, 2M, 1G)\n";
+    std::cout << "  -m, --memory SIZE      Set memory size (e.g., 512K, 512KB, 2M, 2MB, 1G, 1GB)\n";
     std::cout << "                         Default: 1M (1048576 bytes)\n";
     std::cout << "  -h, --help             Show this help message\n";
     std::cout << "\n";
     std::cout << "Examples:\n";
     std::cout << "  " << program_name << " --verbose\n";
     std::cout << "  " << program_name << " --memory 2M\n";
-    std::cout << "  " << program_name << " -m 512K -v\n";
+    std::cout << "  " << program_name << " -m 512KB -v\n";
 }
 
 } // namespace config
