b94eef098bb4627ba4141c5352b09af9c29c5357
Drazdauskas Šarūnas <sarunas.drazdauskas@mif.stud.vu.lt>
2026-01-03 23:16:59 +0200

    add mock sys api test class

--- tests/process_tests.cpp
+++ tests/process_tests.cpp
@@ -1,13 +1,12 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include "process/ProcessManager.h"
-#include "kernel/SysCallsAPI.h"
+#include "testHelpers/MockSysApi.h"
 #include "scheduler/Scheduler.h"
 #include "scheduler/algorithms/PriorityAlgorithm.h"
 #include "scheduler/algorithms/FCFSAlgorithm.h"
 #include "scheduler/algorithms/RoundRobinAlgorithm.h"
 #include "logger/Logger.h"
-#include <map>
 
 using namespace process;
 using namespace scheduler;
@@ -15,93 +14,9 @@ using ::testing::_;
 using ::testing::Return;
 using ::testing::NiceMock;
 
-// Mock SysApi for process manager testing
-class MockSysApi : public sys::SysApi {
-private:
-    std::map<void*, size_t> allocations;
-    
-public:
-    void* allocateMemory(size_t size, int processId = 0) override {
-        void* ptr = new char[size];
-        allocations[ptr] = size;
-        return ptr;
-    }
-    
-    sys::SysResult deallocateMemory(void* ptr) override {
-        auto it = allocations.find(ptr);
-        if (it == allocations.end()) {
-            return sys::SysResult::Error;
-        }
-        delete[] static_cast<char*>(ptr);
-        allocations.erase(it);
-        return sys::SysResult::OK;
-    }
-    
-    void freeProcessMemory(int processId) override {
-        // Mock implementation - just clear allocations for this process
-        // In real implementation, MemoryManager tracks by PID
-    }
-    
-    void scheduleProcess(int, int, int) override {}
-    void unscheduleProcess(int) override {}
-    void suspendScheduledProcess(int) override {}
-    void resumeScheduledProcess(int) override {}
-    
-    ~MockSysApi() {
-        for (auto& [ptr, size] : allocations) {
-            delete[] static_cast<char*>(ptr);
-        }
-    }
-    
-    // Stub implementations
-    sys::SysResult fileExists(const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult readFile(const std::string&, std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult createFile(const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult deleteFile(const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult writeFile(const std::string&, const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult editFile(const std::string&, const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult copyFile(const std::string&, const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult moveFile(const std::string&, const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult appendFile(const std::string&, const std::string&) override { return sys::SysResult::OK; }
-    std::string getWorkingDir() override { return "/"; }
-    sys::SysResult listDir(const std::string&, std::vector<std::string>&) override { return sys::SysResult::OK; }
-    sys::SysResult makeDir(const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult removeDir(const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult changeDir(const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult copyDir(const std::string&, const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult moveDir(const std::string&, const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult saveToDisk(const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult loadFromDisk(const std::string&) override { return sys::SysResult::OK; }
-    sys::SysResult resetStorage() override { return sys::SysResult::OK; }
-    sys::SysResult listDataFiles(std::vector<std::string>&) override { return sys::SysResult::OK; }
-    sys::SysApi::SysInfo getSysInfo() override { return {}; }
-    void requestShutdown() override {}
-    void sendSignal(int) override {}
-    sys::SysResult sendSignalToProcess(int, int) override { return sys::SysResult::OK; }
-    int fork(const std::string&, int, int, int, bool) override { return 0; }
-    std::vector<ProcessInfo> getProcessList() override { return {}; }
-    bool processExists(int) override { return false; }
-    std::string readLine() override { return ""; }
-    void beginInteractiveMode() override {}
-    void endInteractiveMode() override {}
-    bool addCPUWork(int, int) override { return false; }
-    bool waitForProcess(int) override { return false; }
-    bool exit(int, int) override { return false; }
-    bool reapProcess(int) override { return false; }
-    bool isProcessComplete(int) override { return false; }
-    int getProcessRemainingCycles(int) override { return -1; }
-    bool setSchedulingAlgorithm(scheduler::SchedulerAlgorithm, int) override { return false; }
-    bool setSchedulerCyclesPerInterval(int) override { return false; }
-    bool setSchedulerTickIntervalMs(int) override { return false; }
-    bool getConsoleOutput() const override { return false; }
-    void setConsoleOutput(bool) override {}
-    std::string getLogLevel() const override { return "INFO"; }
-    void setLogLevel(logging::LogLevel) override {}
-};
-
 class ProcessManagerMockTest : public ::testing::Test {
 protected:
-    MockSysApi mockSysApi;
+    testHelpers::MockSysApi mockSysApi;
     std::unique_ptr<CPUScheduler> scheduler;
     
     void SetUp() override {
